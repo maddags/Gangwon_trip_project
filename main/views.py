a3749ae99787e5b3e20c9b8fcf228b0a95f4d7d7
maddags <maddags@naver.com>
2022-07-14 17:31:33 +0900

    마지막 파일 :view.py

--- main/views.py
+++ main/views.py
@@ -1,27 +1,30 @@
 from django.shortcuts import redirect, render
-from .models import TripNews, NewsSummery
+from .models import TripNews, NewsSummery, InputKeyword
 import pandas as pd
 import mlflow
 import warnings
-
+import pickle
+import numpy as np
+​
 # Create your views here.
-
-
+​
+​
 def about(request):
     datas = TripNews.objects.all().order_by("created_at")
-    # urls = NewsUrl.objects.all()
     summ = NewsSummery.objects.all()
     return render(request, 'about.html', {'news' : datas, 'summ': summ})
-
-
+​
+​
 def index(request):
     return render(request, 'index.html')
 def post(request):
     return render(request, 'post.html')
 def contact(request):
     return render(request, 'contact.html')
-
-
+def recommend(request):
+    return render(request, 'recommend.html')
+​
+​
 def chart_data(request):
     df = pd.read_csv("./data/place_review_cnt_data.csv")
     df2 = pd.read_csv('./data/word_cnt_info.csv')
@@ -32,34 +35,47 @@ def chart_data(request):
     words = list(df2['단어'][:7])
     word_dict = list(word_dict["장소"])
     word_dict = word_dict
-    return render(request, 'post.html',
-                  {'cnt_review': cnt_review, 'names' : names, 'cnt_word' : cnt_word, 'words' : words,"word_dict" : word_dict
-                  })
-
-
-
-
-# 예측하기
-def prediction(request):
-    place_list = pd.read_csv("../data/Gangwon_place_list.csv", encoding ="utf-8-sig")
-    user = pd.read_csv("../data/user_answer_form.csv")
+    input_keywords = InputKeyword.objects.all().order_by("-count")
+    if len(input_keywords) < 7:
+        input_keywords = input_keywords[:len(input_keywords)]
+    else:
+        input_keywords = input_keywords[:7]
+        
     if request.method == 'POST':
-        selected_place =  request.POST.getlist("loc")
+        selected_place =  request.POST.getlist("CheckboxName")
         print(selected_place)
+​
+        if selected_place != []:    # 선택한 단어 DB에 저장
+            for keyword in selected_place:
+                if InputKeyword.objects.filter(keyword = keyword).count() != 0: # 기존 데이터가 존재할 경우 +1
+                    update_row = InputKeyword.objects.get(keyword = keyword)
+                    update_row.count = update_row.count +1
+                    update_row.save()
+                else:
+                    InputKeyword(keyword = keyword, count = 1).save()
+​
+        place_list = pd.read_csv("./data/Gangwon_place_list.csv", encoding ="utf-8-sig")
+        user = pd.read_csv("./data/user_answer_form.csv")
         for place in selected_place:
             if place in user.columns:
                 user.at[0,place] = 1
-
-    if __name__ == '__main__':
-        warnings.filterwarnings("ignore")
-        logged_model = 'runs:/e47700ff63224201a8473eb7fc3380f5/best_estimator'
-
-        loaded_model = mlflow.pyfunc.load_model(logged_model)
-        test_x = pd.read_csv("../data/example_user1.csv")
-        pred = loaded_model.predict(test_x)
-        
+        loaded_model = pickle.load(open('./models/model.pkl', 'rb'))
+        pred = loaded_model.predict(user)
         predict_places = []
         for loc in pred:
             predict_places.append(place_list.iloc[loc,:])
-    
-    return render(request,"recommend.html",{"predict_places" : predict_places})
+        
+        # print(predict_places[0].str.split(" ")[0])
+        # print('type:', type(predict_places[0].str.split(" ")[0]))            
+        predict_places = predict_places[0].str.split(" ")[0]
+        if type(predict_places) == float:
+            predict_places = ["키워드를 다시 입력해주세요"] 
+        else : 
+            pred_place = predict_places[0]
+        
+        return render(request,"recommend.html",{"predict_places" : predict_places})
+​
+    else :    
+        return render(request, 'post.html',
+                        {'cnt_review': cnt_review, 'names' : names, 'cnt_word' : cnt_word, 'words' : words,"word_dict" : word_dict,  'input_keywords' : input_keywords
+                        })
